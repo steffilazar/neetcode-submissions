12e226c895289941407c57157f954dd00cbdb23e
Steffi Lazar <147672318+steffilazar@users.noreply.github.com>
2026-08-04 00:58:16 +0530

    Add: binary-tree-diameter - submission-4

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-4.py
@@ -0,0 +1,59 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.dia=0
+        def dfs(root):
+
+            if not root :
+                return 0
+
+            l=dfs(root.left)
+            r=dfs(root.right)
+
+            self.dia=max(self.dia,l+r)
+            return 1+ max(l,r)
+        dfs(root)
+        return self.dia
+        
+    #     if not root:
+    #         return 0
+    #     res=0
+
+    #     leftdepth= self.maxdepth(root.left)
+    #     rightdepth= self.maxdepth(root.right)
+    #     dia= leftdepth+rightdepth
+    #     sub =max(self.diameterOfBinaryTree(root.left), self.diameterOfBinaryTree(root.right))
+    #     return max(dia,sub)
+
+    # def maxdepth(self, root: Optional[TreeNode]) -> int:
+    #     if not root:
+    #         return 0
+
+    #     return 1+ max(self.maxdepth(root.left), self.maxdepth(root.right))
+
+        # self.res=0
+
+        # def dfs(node):
+
+        #     if not node :
+        #         return 0
+            
+        #     left=dfs(node.left)
+        #     right=dfs(node.right)
+
+        #     self.res=max(self.res, left+right)
+        #     return 1+ max(left,right)
+
+        # dfs(root)
+        # return self.res
+        
+
+
+
+        
