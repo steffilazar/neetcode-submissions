ebe25813674ae3101e0df843e4f6773b6862f1b2
Steffi Lazar <147672318+steffilazar@users.noreply.github.com>
2026-09-07 20:46:42 +0530

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.py
@@ -0,0 +1,54 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        memo={0:1,1:1}
+
+        def ans(x):
+            if x in memo:
+                return memo[x]
+            else:
+                memo[x]= ans(x-1) + ans(x-2)
+                return memo[x]
+        return ans(n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # memo={0:1, 1:1}
+
+        # def fi(x):
+        #     if x in memo:
+        #         return memo[x]
+        #     else:
+        #         memo[x]=fi(x-2)+fi(x-1)
+        #         return memo[x]
+        # return fi(n)
+
+        first,second=1,1
+        if n==1:
+            return 1
+
+        for i in range(2,n+1):
+            temp=first+second
+            first=second
+            second=temp
+        return temp
